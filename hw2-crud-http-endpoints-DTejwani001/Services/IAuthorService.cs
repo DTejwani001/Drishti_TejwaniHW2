aabe06f9327d20f41fb250b466c4ba2ed4792f2f
DTejwani001 <125599184+DTejwani001@users.noreply.github.com>
2023-09-25 20:10:35 -0400

    Add files via upload

---
+++ hw2-crud-http-endpoints-DTejwani001/Services/IAuthorService.cs
@@ -0,0 +1,8 @@
+public interface IAuthorService
+{
+    IEnumerable<Author> GetAllAuthors();
+    Author GetAuthorById(int id);
+    void AddAuthor(Author author);
+    void UpdateAuthor(Author author);
+    void DeleteAuthor(int id);
+}
